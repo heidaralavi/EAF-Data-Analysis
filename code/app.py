63f0c6c4d2b16e4160398ffc5ad96758af3f7659
heidar alavi <alavi.h@gmail.com>
2024-12-27 15:58:38 +0330

    predict complited

--- code/app.py
+++ code/app.py
@@ -20,43 +20,39 @@ def coke1030_input_features():
     c_coke1030 = st.sidebar.slider('C (%)', 80.0, 85.0, 82.0,key='coke1030_1')
     s_coke1030 = st.sidebar.slider('S (%)', 0.0, 2.0, 1.0,key='coke1030_2')
     s112_coke1030 = st.sidebar.slider('Size 112 (%)', 0.0, 5.0, 2.0,key='coke1030_3')
-    clf = pickle.load(open(f"{working_dir}/trained_models/coke_1030_rfc_model.pkl", 'rb'))
-    data = {'c': c_coke1030,
-            's': s_coke1030,
-            's112': s112_coke1030,
+    #clf = pickle.load(open(f"{working_dir}/trained_models/coke_1030_rfc_model.pkl", 'rb'))
+    data = {'c_coke1030': c_coke1030,
+            's_coke1030': s_coke1030,
+            's112_coke1030': s112_coke1030,
             }
     features = pd.DataFrame(data, index=['p'])
-    features['Coke 1030 Type'] = clf.predict(features)
+    #features['Coke 1030 Type'] = clf.predict(features)
     return features
 
 def cokefine_input_features():
     c_cokefine = st.sidebar.slider('C (%)', 70.0, 100.0, 85.0,key='cokefine_1')
     s_cokefine = st.sidebar.slider('S (%)', 0.0, 2.0, 1.0,key='cokefine_2')
     s05_cokefine = st.sidebar.slider('Size 05 (%)', 0.0, 5.5, 2.0,key='cokefine_3')
-    clf = pickle.load(open(f"{working_dir}/trained_models/coke_fine_rfc_model.pkl", 'rb'))
-    data = {'c': c_cokefine,
-            's': s_cokefine,
-            's05': s05_cokefine,
+    #clf = pickle.load(open(f"{working_dir}/trained_models/coke_fine_rfc_model.pkl", 'rb'))
+    data = {'c_cokefine': c_cokefine,
+            's_cokefine': s_cokefine,
+            's05_cokefine': s05_cokefine,
             }
     features = pd.DataFrame(data, index=['p'])
-    features['Coke Fine Type'] = clf.predict(features)
+    #features['Coke Fine Type'] = clf.predict(features)
     return features
 
 def dolo_input_features():
     cao_dolo = st.sidebar.slider('Cao (%)', 45.0, 60.0, 55.0,key='dolo_1')
     mgo_dolo = st.sidebar.slider('Mgo (%)', 30.0, 40.0, 33.0,key='dolo_2')
     s0_95_dolo = st.sidebar.slider('Size 0-95 (%)', 0.0, 10.0, 2.0,key='dolo_3')
-    s95_385_dolo = st.sidebar.slider('Size 95-385 (%)', 0.0, 100.0, 55.0,key='dolo_4')
-    s385_1000_dolo = st.sidebar.slider('Size 385-1000 (%)', 0.0, 20.0, 2.0,key='dolo_5')
-    clf = pickle.load(open(f"{working_dir}/trained_models/dolomite_rfc_model.pkl", 'rb'))
-    data = {'cao': cao_dolo,
-            'mgo': mgo_dolo,
-            's095': s0_95_dolo,
-            's95_385': s95_385_dolo,
-            's385_1000': s385_1000_dolo,
+    #clf = pickle.load(open(f"{working_dir}/trained_models/dolomite_rfc_model.pkl", 'rb'))
+    data = {'cao_dolo': cao_dolo,
+            'mgo_dolo': mgo_dolo,
+            's095_dolo': s0_95_dolo,
             }
     features = pd.DataFrame(data, index=['p'])
-    features['Dolomite Type'] = clf.predict(features)
+    #features['Dolomite Type'] = clf.predict(features)
     return features
 
 
@@ -67,21 +63,30 @@ def dri_input_features():
     c_dri = st.sidebar.slider('C (%)', 0.0, 3.0, 1.2,key='dri_4')
     gunge_dri = st.sidebar.slider('Gunge (%)', 7.0, 15.0, 9.5,key='dri_11')
     feo_dri = st.sidebar.slider('Feo (%)', 6.0, 15.0, 9.5,key='dri_12')
-    feo_c_dri = st.sidebar.slider('Feo_c (%)', 0.0, 12.0, 9.5,key='dri_13')
-    clf = pickle.load(open(f"{working_dir}/trained_models/dri_rfc_model.pkl", 'rb'))
+    #clf = pickle.load(open(f"{working_dir}/trained_models/dri_rfc_model.pkl", 'rb'))
     data = {'fe_metal': fe_metal_dri,
             'fe_total': fe_total_dri,
             'md': md_dri,
             'c': c_dri,
             'gunge': gunge_dri,
             'feo': feo_dri,
-            'feo_c': feo_c_dri,
-                        }
+            }
     features = pd.DataFrame(data, index=['p'])
-    features['Dri Type'] = clf.predict(features)
+    #features['Dri Type'] = clf.predict(features)
     return features
 
-
+def lime_input_features():
+    cao_lime = st.sidebar.slider('Cao (%)', 45.0, 60.0, 55.0,key='lime_1')
+    mgo_lime = st.sidebar.slider('Mgo (%)', 30.0, 40.0, 33.0,key='lime_2')
+    s0_95_lime = st.sidebar.slider('Size 0-95 (%)', 0.0, 10.0, 2.0,key='lime_3')
+    #clf = pickle.load(open(f"{working_dir}/trained_models/dolomite_rfc_model.pkl", 'rb'))
+    data = {'cao_lime': cao_lime,
+            'mgo_lime': mgo_lime,
+            's095_lime': s0_95_lime,
+            }
+    features = pd.DataFrame(data, index=['p'])
+    #features['Dolomite Type'] = clf.predict(features)
+    return features
 
 
 st.sidebar.header('Input Coke (1030) Parameters:')
@@ -92,7 +97,25 @@ st.sidebar.header('Input Dolomite Parameters:')
 dolo_df = dolo_input_features()
 st.sidebar.header('Input DRI Parameters:')
 dri_df = dri_input_features()
+st.sidebar.header('Input Lime Parameters:')
+lime_df = lime_input_features()
+
 
 st.subheader('User Input parameters')
-st.write(pd.concat([coke_1030_df['Coke 1030 Type'],coke_fine_df['Coke Fine Type'],dolo_df['Dolomite Type']
-                    ,dri_df['Dri Type']],axis=1))
+prediction_df = pd.concat([coke_1030_df,coke_fine_df,dolo_df,dri_df,lime_df],axis=1)
+st.write(prediction_df)
+
+clf = pickle.load(open(f"{working_dir}/trained_models/all_feed_rfc_model.pkl", 'rb'))
+predict_label = clf.predict(prediction_df.values).tolist()[0]
+st.write(f"Predicted Label: {predict_label}")
+df = pd.read_csv(f"{working_dir}/data/all_data_with_labels.csv")
+mask = df.columns.str.contains('Coke1030|CokeFine|Dolomite|DRI|Lime')
+df = df[df.columns[~mask]]
+mask2 = df['Total_labels'] == predict_label
+df = df[mask2]
+max_b2 = df['b2 (Slag)'].max()
+mask3 = df['b2 (Slag)'] == max_b2
+df = df[mask3]
+mask4 = df.columns.str.contains('EAF|Heat|b2|Total_labels')
+df = df[df.columns[mask4]]
+st.write(df)
