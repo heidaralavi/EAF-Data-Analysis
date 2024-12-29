ed276814ab908f02b4d8a78c67dbdcfd35fd2033
heidar alavi <alavi.h@gmail.com>
2024-12-29 21:24:16 +0330

    update

--- opencv-codes/opencv.py
+++ opencv-codes/opencv.py
@@ -1,5 +1,9 @@
+import os
 import cv2 as cv
-img = cv.imread("/app/opencv-images/photo15978847178.jpg")
+working_dir = os.getcwd()
+#working_dir ='..'  # Use on Jupyter Notebook
+
+img = cv.imread(f"{working_dir}/opencv-images/isotermal-b3-1_5.jpg")
 
 #cv.imshow("Display window", img)
 #k = cv.waitKey(0) # Wait for a keystroke in the window
